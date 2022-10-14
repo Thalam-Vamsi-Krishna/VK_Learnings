ec51c4d6f07d8926d3f036821a2fcbca464842e7
Thalam-Vamsi-Krishna <vamsikrishna.thalam@gmail.com>
2022-10-14 18:42:20 +0530

    querySelector & querySelectorAll implemented

--- dom.js
+++ dom.js
@@ -6,10 +6,16 @@ headerTitle.textContent = "Item Lister";
 var item = document.getElementsByClassName("list-group-item");
 for (var i = 0; i < item.length; i++) {
   item[i].style.fontWeight = "bold";
-  item[i].style.backgroundColor = "yellow";
+  item[i].style.backgroundColor = "#f4f4f4";
 }
-item[2].style.backgroundColor = "green";
-var new_item = document.getElementsByClassName("list-new-item");
-var new_item_tag = document.getElementsByTagName("list-new-item");
-new_item.style.backgroundcolour = "red";
-new_item_tag.style.fontWeight = "bold";
+//item[2].style.backgroundColor = "green";
+var li = document.getElementsByTagName("li");
+//li[1].style.backgroundColor = "red";
+//li[2].style.display = "none";
+var odd = document.querySelectorAll("li:nth-child(odd)");
+var even = document.querySelectorAll("li:nth-child(even)");
+even[0].style.color = "green";
+for (var i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = "green";
+  even[i].style.backgroundColor = "red";
+}
